6487c65ac8178007062ac5c0b2a445968e2c4f2e
Mospanko <93765666+Mospanko@users.noreply.github.com>
2021-11-28 15:59:21 +0300

    Add files via upload

---
+++ Лаба 1 задание 3/Лаба 1 задание 3/Лаба 1 задание 3.cpp
@@ -0,0 +1,48 @@
+﻿// Лаба 1 задание 3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+char* strcat(char* dest, char* src)
+{
+	int count1 = 0; // считаем кол-во символов в исходнике
+	while (dest[count1] != 0)
+	{
+		count1++;
+	}
+
+
+	int count2 = 0;
+	while (src[count2] != 0)
+	{ // дописываем в конец первой строки символы второй строки
+		dest[count1] = src[count2];
+		count1++;
+		count2++;
+	}
+	return dest;
+    
+}
+void E3()
+{
+    char src[] = "Hello Ivan Savenkov";
+    char dest[128] = "I'm Ilya Mospanko & ";
+
+    char* outsided = strcat(dest, src);
+    printf("\nмоя функция: %s", outsided);
+
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	printf("Нажмите 3\n");
+	int choice = 0;
+	cin >> choice;
+	switch (choice)
+	{
+	case 3: system("cls"); E3();
+		break;
+		return 0;
+	}
+}
