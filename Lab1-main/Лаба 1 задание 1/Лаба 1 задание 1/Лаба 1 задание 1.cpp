9ea58f9fefaf59f90d444275d580ce3290caa485
Mospanko <93765666+Mospanko@users.noreply.github.com>
2021-12-02 18:07:05 +0300

    Add files via upload

---
+++ Lab1-main/Лаба 1 задание 1/Лаба 1 задание 1/Лаба 1 задание 1.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <string>
+#include <utility>
+
+/*Объявить массив из n=20 целых чисел, проинициализировать нулями. Функция
+processArray() должна заполнить массив случайными числами от одного до пяти,
+вычислить и вернуть наименее часто встречающееся значение в массиве (если таких
+несколько, вернуть наименьшее из них) и сформировать выходной массив из всех
+элементов входного, отсортированных в порядке возрастания. Вывести на экран
+массивы.
+*/
+using namespace std;
+
+void printArr(int* arr, size_t N, string s) {
+    printf(" %s", s.c_str());
+    printf("\n[");
+    for (size_t i = 0; i < N; ++i)
+    {
+        printf(" %i ", arr[i]);        
+    }
+    printf("]\n");    
+}
+int process(int* arr, int* res, size_t N) {
+    for (size_t i = 0; i < N; ++i)              //заполнение исходного массива
+    {
+        arr[i] = ((rand() % 5) + 1);
+        res[i] = arr[i];
+    }
+    double rareliest = 0.0;
+    int help[5] = { 0 };
+
+    for (size_t i = 0; i < N; i++)            //работа с выходным массивом
+    {
+        int smallest_ind = i;
+        for (size_t j = i + 1; j < N; j++)
+        {
+            if (res[j] < res[smallest_ind])
+            {
+                smallest_ind = j;
+            }
+        }
+        swap(res[i], res[smallest_ind]);
+    }
+
+    for (size_t i = 0; i < N; ++i) {            //выборка наименьшего
+        help[arr[i] - 1]++;
+    }
+    int min = help[0];
+    int min_ind = 0;
+    for (int i = 0; i < 5; ++i)
+    {
+        if (help[i] < min)
+        {
+            min = help[i];
+            min_ind = i;
+        }
+    }
+
+    return min_ind + 1;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    const size_t N = 20;
+    int arr[N] = { 0 };
+    int res[N] = { 0 };
+    srand(0);
+    int rareliest = process(arr, res, N);
+    printArr(arr, N, "Вход");
+    printArr(res, N, "Выход");
+    printf("Наименее часто встречающееся: %i", rareliest);
+}
---
+++ Lab1-main/Лаба 1 задание 2/Лаба 1 задание 2/Лаба 1 задание 2.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <cmath>
+
+/*Преобразование: 2D → 1D. Двумерный массив 5х3 вещественных чисел необходимо
+выложить в один ряд по элементам по столбцам сверху вниз.
+Инициализация: заполнить массив числами x[i][j] = sin(i – j) + cos(i + j).
+Вывод на экран: на каждый элемент массива отвести 10 позиций.*/
+
+using namespace std;
+
+double* MakeArr1D(double** arr, size_t rows, size_t cols) {
+    double* arr1D = new double[rows * cols];
+    for (size_t i = 0; i < rows * cols; i++)
+    {
+        for (size_t j = 0; j < rows; j++)
+        {
+            for (size_t l = 0; l < cols; l++)
+            {
+                *(arr1D + i) = *(*(arr + j) + l);
+            }
+        }
+    }
+    return arr1D;
+}
+void freeArray1D(double* arr)
+{
+    delete[] arr;
+}
+
+void freeArray2D(double** arr2D, size_t N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        delete[] arr2D[i];
+    }
+    delete[] arr2D;
+}
+
+void PrintArr2D(double** arr, size_t rows, size_t cols)
+{
+    for (size_t i = 0; i < rows; i++)
+    {
+        for (size_t j = 0; j < cols; j++)
+        {
+            cout << *(*(arr + i) + j) << endl;
+        }
+    }
+}
+void PrintArr1D(double* arr, size_t N) {
+    printf("\n[");
+    for (size_t i = 0; i < N; ++i)
+    {
+
+        printf(" %d ", arr[i]);
+        
+    }
+    printf("]\n");
+}
+void InitializeArr(double** arr, size_t rows, size_t cols) {
+
+    
+    for (size_t i = 0; i < rows; i++)
+    {
+        for (size_t j = 0; j < cols; j++)
+        {
+            *(*(arr + i) + j) = sin(i - j) + cos(i + j);
+        }
+    }
+}
+int main()
+{
+    const size_t LEN = 15;
+    const size_t M = 3;
+    const size_t N = 5;
+
+    double** arr = new double* [N];
+    for (size_t i = 0; i < N; i++)
+    {
+        *(arr + i) = new double[M];
+    }
+    InitializeArr(arr, N, M);
+    PrintArr2D(arr, N, M);
+    double* arr_res = MakeArr1D(arr, N, M);
+    PrintArr1D(arr_res, 15);
+}
+
